5822f7cbbe5f912373544d22dbb261a65b744abf
joao <joaopaz79@outlook.com>
2023-03-30 00:34:00 -0300

    Finalzia requisito 6.

---
+++ app/backend/src/database/models/usersModel.ts
@@ -0,0 +1,45 @@
+import { Model, InferAttributes, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model<InferAttributes<Users>> {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init(
+  {
+    id: {
+      type: DataTypes.NUMBER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: false,
+    sequelize: db,
+    timestamps: false,
+    modelName: 'users',
+  },
+);
+
+export default Users;
